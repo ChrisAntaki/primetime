839e7746741cf6370925da82084659bde586568d
Chris Antaki <ChrisAntaki@gmail.com>
2014-06-21 16:07:46 -0700

    Added saving functionality

--- primetime.go
+++ primetime.go
@@ -1,7 +1,35 @@
 package main
 
+import (
+	"bufio"
+	"math"
+	"os"
+	"strconv"
+)
+
+var maximum_saved float64 = math.Inf(1)
+
 func Generate(ch chan<- int) {
-	for i := 2; ; i++ {
+	i := 0
+
+	loaded_channel := make(chan int)
+	go LoadDataFile(loaded_channel)
+	var number int
+	for {
+		number = <-loaded_channel
+		if number == -1 {
+			maximum_saved = float64(i)
+			break
+		}
+		i = number
+		ch <- i
+	}
+
+	if i < 2 {
+		i = 2
+	}
+
+	for ; ; i++ {
 		ch <- i // Send 'i' to channel 'ch'.
 	}
 }
@@ -17,15 +45,45 @@ func Filter(in <-chan int, out chan<- int, prime int) {
 	}
 }
 
+func LoadDataFile(ch chan<- int) {
+	file, _ := os.Open("data.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		number, _ := strconv.Atoi(scanner.Text())
+		ch <- number
+	}
+	ch <- -1
+}
+
+func AppendToDataFile(data string) {
+	filename := "data.txt"
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	if _, err = f.WriteString(data + "\n"); err != nil {
+		panic(err)
+	}
+}
+
 // The prime sieve: Daisy-chain Filter processes.
 func main() {
+
 	ch := make(chan int) // Create a new channel
 	go Generate(ch)      // Launch Generate goroutine.
-	for i := 0; i < 100; i++ {
-		prime := <-ch
-		print(prime, "\n")
+	var prime int
+	for i := 0; i < 500; i++ {
+		prime = <-ch
+		if float64(prime) > maximum_saved {
+			AppendToDataFile(strconv.Itoa(prime))
+		}
 		ch1 := make(chan int)
 		go Filter(ch, ch1, prime)
 		ch = ch1
 	}
+	print("Largest prime so far is ", prime)
 }
