b29d54e1f146df2582bc1045b18857cdbd98266a
Chris Antaki <ChrisAntaki@gmail.com>
2014-07-08 00:55:11 -0700

    Added a faster implementation

---
+++ slicetime.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+)
+
+func main() {
+	nth := GetNth()
+
+	primes := []int{2}
+	for i := 3; len(primes) < nth; i++ {
+		prime := true
+
+		for j := 0; j < len(primes) && primes[j]*primes[j] <= i; j++ {
+			if i%primes[j] == 0 {
+				prime = false
+				break
+			}
+		}
+
+		if prime {
+			primes = append(primes, i)
+		}
+	}
+
+	prime := primes[len(primes)-1]
+
+	print(prime)
+}
+
+func GetNth() int {
+	var nth = flag.Int("nth", 100, "Find the Nth prime. (Default: 100)")
+	flag.Parse()
+	return *nth
+}
+
+func dealbreaker(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
