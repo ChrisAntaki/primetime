6e4e073d3320ad5395d5766649664a28c2b2f006
Chris Antaki <ChrisAntaki@gmail.com>
2014-07-21 02:46:32 -0700

    Added an example using C arrays

---
+++ arraytime.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int nth = 100;
+  
+  if (argc == 2)
+  {
+    nth = atoi(argv[1]);
+  }
+
+  int primesFound = 1;
+  int primes[nth];
+  primes[0] = 2;
+
+  int i, isPrime;
+  int candidate = 3;
+  while (primesFound < nth)
+  {
+    isPrime = 1;
+
+    for (i = 0; i < primesFound && primes[i]*primes[i] <= candidate; i++)
+    {
+      if (candidate % primes[i] == 0)
+      {
+        isPrime = 0;
+        break;
+      }
+    }
+
+    if (isPrime)
+    {
+      primes[primesFound] = candidate;
+      primesFound++;
+    }
+
+    candidate += 2;
+  }
+
+  printf("%d\n", primes[primesFound - 1]);
+
+  return 0;
+}
